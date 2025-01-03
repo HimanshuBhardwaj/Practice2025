fc8509b47c9ba591d5f0a013587b28c5f09aa74a
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2025-01-03 16:50:28 +0530

    minor changes

---
+++ src/main/java/org/example/jan03/ThreadPOC.java
@@ -0,0 +1,63 @@
+package org.example.jan03;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ThreadPOC {
+    public static void main(String[] args) throws InterruptedException {
+        int n=100;
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        ArrayList<ThreadOps> threadOps = new ArrayList<>();
+
+        for (int i=0;i<n;i++) {
+            threadOps.add(new ThreadOps(i,threadOps,atomicInteger,n));
+        }
+
+        for (ThreadOps t: threadOps) {
+            t.start();
+        }
+
+        Thread.sleep(1000);
+
+        for (ThreadOps t: threadOps) {
+            t.interrupt();
+        }
+        Thread.sleep(10000);
+    }
+}
+
+
+//16:23
+class ThreadOps extends Thread {
+    int index;
+    ArrayList<ThreadOps> threads;
+    int numThreads;
+    final AtomicInteger num;
+
+    public ThreadOps(int index, ArrayList<ThreadOps> threads, AtomicInteger num, int n) {
+        this.index=index;
+        this.numThreads = n;
+        this.threads = threads;
+        this.num = num;
+    }
+
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            synchronized (num) {
+                if (num.intValue() % numThreads != index) {
+                    try {
+                        num.wait();
+                    } catch (InterruptedException e) {
+                        System.out.println("Thread "+index+" interrupted");
+                        return;
+                    }
+                } else {
+                    System.out.println(num.intValue() + "\t" + index + "\t" + (num.intValue() % numThreads));
+                    num.incrementAndGet();
+                    num.notifyAll();
+                }
+            }
+        }
+    }
+
+}
